37d9015208bcebab12841331e296fca71341b55e
Hal Jordan <jordaha@win.eng.vzwnet.com>
2019-06-26 17:20:57 -0500

    added print for test

--- moveplease01.py
+++ moveplease01.py
@@ -9,3 +9,5 @@ shutil.move('raynor.obj', 'ceph_storage/')
 xname = input('What is the new name for kerrigan.obj? ')
 shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
 
+print('hi')
+
